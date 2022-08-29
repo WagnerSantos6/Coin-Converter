7dcfe26b65af8b004be29fea6c8debd1a75dd9cb
Wagner Santos <wagnerstss@gmail.com>
2022-08-29 11:48:14 -0300

    Coin Converter

---
+++ Exercicio9/ConversorDeMoeda.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Globalization;
+
+namespace Exercicio9
+{
+    internal class ConversorDeMoeda
+    {
+       public static double Iof = 0.6;
+        public static double Conversao(double valor, double cotacao)
+        {
+            return valor * cotacao + valor * Iof;
+        }  
+        
+    }
+}
---
+++ Exercicio9/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Globalization;
+
+namespace Exercicio9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Qual é a cotação do dólar? ");
+            double cotacao = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine();
+            Console.WriteLine("Quantos dólares você vai comprar? ");
+            double valor = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            double result = ConversorDeMoeda.Conversao(valor, cotacao);
+            Console.WriteLine();
+            Console.WriteLine("Valor a pagar: $" + result.ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine();
+
+           
+        }
+    }
+}
